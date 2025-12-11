9ffb611034a2e84cb492bd594ce37a94d397ad29
vanstrong4 <alfeushendrievan@gmail.com>
2025-12-11 22:55:55 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tugas_week_10_1123150186/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_week_10_1123150186
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Vanstore());
+}
+
+class Vanstore extends StatelessWidget {
+  const Vanstore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      theme: ThemeData(fontFamily: 'BebasNeue'),
+      title: 'evan tugas week 10',
+    );
+  }
+}
